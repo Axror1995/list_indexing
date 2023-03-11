fadc1acc2f94ecb6eecaf1db4ece157b20bea8ba
Axror1995 <axrorraximov108@gmail.com>
2023-03-11 16:50:11 +0900

    list08.dart

--- bin/list_index08.dart
+++ bin/list_index08.dart
@@ -6,4 +6,24 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1) {
+  int a = list1.length;
+  int b = 0;
+  String ans = 'folse';
+  String an = 'true';
+  while (b < a) {
+    if (list1[b] == 0) {
+      list1[b] = ans;
+    }
+    if (list1[b] == 1) {
+      list1[b] = an;
+    }
+
+    b++;
+  }
+  return list1;
+}
+
+void main() {
+  print(func([0, 0, 1, 1, 0, 1]));
+}
